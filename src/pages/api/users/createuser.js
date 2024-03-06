9319ffee71cc15d6e046633c305b818dfc437631
soundhar <soundharnamakkal@gmail.com>
2024-03-05 20:16:42 -0500

    login done

--- src/pages/api/users/createuser.js
+++ src/pages/api/users/createuser.js
@@ -1,36 +1,74 @@
-import db from '@/lib/db'
+import db from '@/lib/db';
 import { withAuth } from '../withAuth';
+import bcrypt from 'bcrypt';
 
 export default withAuth(async (req, res) => {
-    if(req.method == 'POST'){
+    if (req.method === 'POST') {
         try {
-            const { name, nickname, password, phonenumber, country, about, activeButton  } = req.body;
+            const { formValues, activeButton } = req.body;
 
-            console.log("this isnthe stuff", name)
-            
+            const { name, nickname, password, phonenumber, country, about } = formValues
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
             await db.beginTransaction();
 
-            // // Executing query 1
-            // let response = await new Promise((resolve, reject) => {
-            //     db.query(sql, values, function (err, results, fields) {
-            //     if (err) reject(err);
-            //     else resolve(results);
+            let sql, sql1;
+
+            if (activeButton === 1) {
+                sql = `INSERT INTO users (username, password, role) VALUES (?, ?, ?)`;
+                // sql1 = `INSERT INTO players_profile (userId, player_name, nick_name, phone_number, country, about) VALUES (?, ?, ?, ?, ?, ?)`;
+            } else {
+                sql = `INSERT INTO users (username, password, role) VALUES (?, ?, ?)`;
+                // sql1 = `INSERT INTO host_profile (userId, host_name, nick_name, phone_number, country, about) VALUES (?, ?, ?, ?, ?, ?)`;
+            }
+
+            const values = [nickname, hashedPassword, activeButton === 1 ? 2 : 3];
+            const response = await new Promise((resolve, reject) => {
+                db.query(sql, values, function (err, results, fields) {
+                    if (err) reject(err);
+                    else resolve(results);
+                });
+            });
+
+            console.log(response.insertId)
+
+            if (activeButton === 1) {
+                // sql = `INSERT INTO users (username, password, role) VALUES (?, ?, ?)`;
+                sql1 = `INSERT INTO players_profile (userId, player_name, nick_name, phone_number, country, about) VALUES (?, ?, ?, ?, ?, ?)`;
+            } else {
+                // sql = `INSERT INTO users (username, password, role) VALUES (?, ?, ?)`;
+                sql1 = `INSERT INTO host_profile (userId, host_name, nick_name, phone_number, country, about) VALUES (?, ?, ?, ?, ?, ?)`;
+            }
+           
+            let userId = response.insertId
+            const values2 = [userId, name, nickname, phonenumber, country, about];
+            const response1 = await new Promise((resolve, reject) => {
+                db.query(sql1, values2, function (err, results, fields) {
+                    if (err) reject(err);
+                    else resolve(results);
+                });
+            });
+           
+            // console.log
+            // const sql2 = `UPDATE players_profile SET userId = ? WHERE nickname = ?`
+            // const values3 = [user, nickname]
+            // const response2 = await new Promise((resolve, reject) => {
+            //     db.query(sql2, values3, function (err, results, fields) {
+            //         if (err) reject(err);
+            //         else resolve(results);
             //     });
             // });
 
+
             await db.commit();
-            res.status(200).json({ status: 'success', message: 'Account created successfully', result: response });
+            res.status(200).json({ status: 'success', message: 'Account created successfully', result: response1 });
         } catch (err) {
-          
-          // If any one query got an error, all the previously ran queries will be reverted
-          console.error(err);
-          await db.rollback();
-        //   if(err.code === 'ER_DUP_ENTRY'){
-        //     errMessage = "Customer with same name already present"
-        //   }
-          res.status(500).json({ status : 'fail', message: errMessage });
+            console.error(err);
+            await db.rollback();
+            res.status(500).json({ status: 'fail', message: err.message });
         }
-    }else{
-        res.status(401 ).json({ status : 'fail', message: 'Not allowed' });
+    } else {
+        res.status(401).json({ status: 'fail', message: 'Not allowed' });
     }
-})
+});
